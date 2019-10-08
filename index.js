3eb33f7cdc84eda1e1ca55d2112bcc7b64c8c412
FredPrudent <fredericbourgain@yahoo.com>
2019-10-08 23:57:33 +0200

    Implémentation du dossier de démarrage de l'API

--- index.js
+++ index.js
@@ -1,31 +1,18 @@
-const mongoose = require('mongoose');
-const express = require ('express');
+const winston = require("winston");
+const express = require("express");
+const config = require("config");
 const app = express();
 
-const clients = require ('./routes/clients');
-const collaborators = require ('./routes/collaborators');
-const equipments = require ('./routes/equipments');
-const pictures = require ('./routes/pictures');
-const posts = require ('./routes/posts');
-const professional_datas = require ('./routes/professional_datas');
+require("./startup/logging")();
+require("./startup/cors")(app);
+require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
-//Connexion BDD
-mongoose.connect('mongodb+srv://oclockteam:oclock2019;@borisdoyeproject-yznva.mongodb.net/admin?retryWrites=true&w=majority')
-    .then(() => console.log ('Connecté à MongoDB'))
-    .catch(err=> console.error ("Connexion échouée à MongoDB...", err));
+const port = process.env.PORT || config.get("port");
+const server = app.listen(port, () =>
+  winston.info(`Listening on port ${port}...`)
+);
 
-app.use(express.json());
-app.use('/api/clients', clients);
-app.use('/api/collaborators', collaborators);
-app.use('/api/equipments', equipments);
-app.use('/api/pictures', pictures);
-app.use('/api/posts', posts);
-app.use('/api/professional_datas', professional_datas);
-
-
-
-
-//PORT
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Ecoute sur le port ${port}...`));
-
+module.exports = server;
